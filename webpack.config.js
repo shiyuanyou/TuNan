fb1017887181c5538d2f2c60dc68c575f7e15d10
shiyuanyou <1604989334@qq.com>
2024-12-25 23:13:58 +0800

    fix 1

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './popup/popup.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'popup/dist'),
+    path: path.resolve(__dirname, 'popup'),
   },
   mode: 'production',
   resolve: {
