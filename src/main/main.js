0d43f6785419f25660844b125e30930424f208cf
shiyuanyou <1604989334@qq.com>
2024-12-31 23:14:40 +0800

    chroe: use eletron

---
+++ src/main/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  // 开发环境下连接 Vite 开发服务器
+  win.loadURL('http://localhost:5173')
+  
+  // 打开开发工具以便调试
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+}) 
