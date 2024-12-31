5ffe71a6404f126936665c617093281d8cedd489
shiyuanyou <1604989334@qq.com>
2024-12-31 23:45:28 +0800

    chore: 调试模式

--- src/main/main.js
+++ src/main/main.js
@@ -11,11 +11,16 @@ function createWindow() {
     }
   })
 
-  // 根据环境加载不同的URL
-  if (process.env.NODE_ENV === 'development') {
+  // 检查是否存在 VITE_DEV_SERVER_URL 环境变量
+  const isDev = process.env.npm_lifecycle_event === 'dev'
+  
+  if (isDev) {
+    // 开发环境
     win.loadURL('http://localhost:5173')
+    // 自动打开开发者工具
     win.webContents.openDevTools()
   } else {
+    // 生产环境
     win.loadFile(path.join(__dirname, '../../dist/index.html'))
   }
 }
